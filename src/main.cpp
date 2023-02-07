0958e20f50c02ae95d544230b5793ba26ee04dc5
t3k <anoop.k.nair@gmail.com>
2023-02-06 22:37:38 -0600

    added cmd line

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "argh.h"
 
-int main()
+int main(int, char* argv[])
 {
+    argh::parser cmdl(argv);
+
+    if (cmdl[{ "-v", "--verbose" }])
+        std::cout << "Verbose, I am.\n";
+
     std::cout << "Hello World!" << std::endl;
+
+    return EXIT_SUCCESS;
 }
